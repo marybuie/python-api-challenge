d3d9d26ab0750228797fbee9336079d0412005bb
Mary Buie <maryelizabethbuie@gmail.com>
2023-09-28 20:52:09 -0500

    final draft

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2f25eb66610f9105ff15e0d195d9129f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "8543eedd57a742329a3dcb6ae4f1c654"
